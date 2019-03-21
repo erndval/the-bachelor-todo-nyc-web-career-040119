694b2a92378e093895eaf3601900337e92ec4d97
Ernie Val <erndval@gmail.com>
2019-03-21 22:40:39 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,6 +6,7 @@ def get_first_name_of_season_winner(data, season)
       cour_data.each do |contestant|
         contestant.each do |key, value|
           if key == "status" && value == "Winner"
+            binding.pry
             return contestant[:name].split(" ")[0]
           end
         end
