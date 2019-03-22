e60f62d83a0934da7a07ed23dbef1d060edd8231
Ernie Val <erndval@gmail.com>
2019-03-22 00:18:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,74 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |cour, cour_data|
+    if cour == season
+      cour_data.each do |contestant|
+        contestant.each do |key, value|
+          if key == "status" && value == "Winner"
+            return contestant["name"].split(" ")[0]
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |cour, cour_data|
+      cour_data.each do |contestant|
+        contestant.each do |key, value|
+          if key == "occupation" && value == occupation
+            return contestant["name"]
+          end
+        end
+      end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |cour, cour_data|
+      cour_data.each do |contestant|
+        contestant.each do |key, value|
+          if key == "hometown" && value == hometown
+            counter += 1
+          end
+        end
+      end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |cour, cour_data|
+      cour_data.each do |contestant|
+        contestant.each do |key, value|
+          if key == "hometown" && value == hometown
+            return contestant["occupation"]
+          end
+        end
+      end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  age_array = []
+  data.each do |cour, cour_data|
+    if cour == season
+      cour_data.each do |contestant|
+        contestant.each do |key, value|
+          if key == "age"
+            age_array << value
+          end
+        end
+      end
+    end
+  end
+  age_f_array = age_array.map(&:to_f)
+  age_f_array.each do |age|
+  sum += age  
+  end
+  (sum/age_f_array.size).round
 end
